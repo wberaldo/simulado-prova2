a0e82f5011aba87d5368b9f38d3097bbf5012eca
aluno do unilasalle <aluno@LB126UNI.ACADEMICO>
2017-04-17 20:04:52 -0300

    AV1

---
+++ prog.c
@@ -0,0 +1,18 @@
+#include <stdio.>
+//prova de lab SO
+
+int main()
+{
+ // declara variaveis
+ int x, y, soma;
+ 
+ //calcula a soma
+ x = 9; y = 8;
+ soma = x + y;
+
+ //imprime
+ printf(soma);
+
+ return 0;
+
+}
